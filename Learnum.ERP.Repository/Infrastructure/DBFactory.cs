73e3befb2f68c2744de1e4d4c8de9e8d548d0ffb
developer@cloudstine.com <developer@cloudstine.com>
2024-06-12 17:26:30 +0530

    Model And Repository

---
+++ Learnum.ERP.Repository/Infrastructure/DBFactory.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Learnum.ERP.Shared.Core;
+
+namespace Learnum.ERP.Repository.Infrastructure
+{
+    public class DBFactory
+    {
+        private IDbConnection _dbConnection;
+
+        //common 
+        public IDbConnection GetConnection(string connectionString)
+        {
+            _dbConnection = new SqlConnection(connectionString);
+            return _dbConnection;
+        }
+
+        //application wise
+        public IDbConnection GetCoreConnection()
+        {
+            _dbConnection = new SqlConnection(ApplicationSettings.CoreConnectionString);
+            return _dbConnection;
+        }
+
+        public IDbCommand GetCommand()
+        {
+            return new SqlCommand();
+        }
+
+        ~DBFactory()
+        {
+            CloseConnection();
+        }
+
+        public void CloseConnection()
+        {
+            if (_dbConnection != null && _dbConnection.State == ConnectionState.Open)
+            {
+                try
+                {
+                    _dbConnection.Close();
+                    _dbConnection.Dispose();
+                }
+                catch { }
+            }
+        }
+    }
+}
