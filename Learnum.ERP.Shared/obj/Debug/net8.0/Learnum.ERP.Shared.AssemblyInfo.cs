ce9c1c23b537ecd587d2f494830c9d35d8679970
developer@cloudstine.com <developer@cloudstine.com>
2024-06-24 16:23:16 +0530

    changas

--- Learnum.ERP.Shared/obj/Debug/net8.0/Learnum.ERP.Shared.AssemblyInfo.cs
+++ Learnum.ERP.Shared/obj/Debug/net8.0/Learnum.ERP.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Learnum.ERP.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd7f2258b2301e7291e6fb052d10d29929cc7d0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a396f1c51b6f8d3847a23d85422a7498dad5c79")]
 [assembly: System.Reflection.AssemblyProductAttribute("Learnum.ERP.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Learnum.ERP.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
