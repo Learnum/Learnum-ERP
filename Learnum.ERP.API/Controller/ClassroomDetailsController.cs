48d287b8804becfe476a31f425b12bdc01efa2f3
developer@cloudstine.com <developer@cloudstine.com>
2024-06-26 04:55:22 +0530

    API Changes

---
+++ Learnum.ERP.API/Controller/ClassroomDetailsController.cs
@@ -0,0 +1,56 @@
+﻿using Learnum.ERP.API.Controller.Branch;
+using Learnum.ERP.Repository.Master;
+using Learnum.ERP.Shared.Core;
+using Learnum.ERP.Shared.Entities.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Learnum.ERP.API.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClassroomDetailsController : ControllerBase
+    {
+        private readonly IClassroomDetailsRepository  classroomDetailsRepository;
+        private readonly ILogger<ClassroomDetailsController> logger;
+
+        public ClassroomDetailsController(
+            ILogger<ClassroomDetailsController> _logger,
+            IClassroomDetailsRepository _classroomDetailsRepository)
+        {
+            logger = _logger;
+            classroomDetailsRepository = _classroomDetailsRepository;
+        }
+
+        [HttpPost("InsertClassroomDetails")]
+        public async Task<IActionResult> InsertClassroomDetails(ClassroomDetailsModel classroomDetailsModel)
+        {
+            if (classroomDetailsModel == null)
+            {
+                return BadRequest("Object is null");
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Invalid model object");
+            }
+
+            var result = await classroomDetailsRepository.InsertClassroomDetails(classroomDetailsModel);
+            if (result == ResponseCode.Success || result == ResponseCode.Updated)
+            {
+                return Ok(result);
+            }
+            return BadRequest("Failed to Save");
+        }
+
+        [HttpGet("getAllClassroomList")]
+        public async Task<IActionResult> GetClassroomDetailsList()
+        {
+            var data = await classroomDetailsRepository.GetClassroomDetailsList();
+            if (data != null)
+            {
+                return Ok(data);
+            }
+            return NotFound("No record found");
+        }
+    }
+}
---
+++ Learnum.ERP.API/Controller/SubjectDetailsController.cs
@@ -0,0 +1,56 @@
+﻿using Learnum.ERP.API.Controller.Branch;
+using Learnum.ERP.Repository.Master;
+using Learnum.ERP.Shared.Core;
+using Learnum.ERP.Shared.Entities.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Learnum.ERP.API.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubjectDetailsController : ControllerBase
+    {
+        private readonly ISubjectDetailsRepository subjectDetailsRepository;
+        private readonly ILogger<SubjectDetailsController> logger;
+
+        public SubjectDetailsController(
+            ILogger<SubjectDetailsController> _logger,
+            ISubjectDetailsRepository _subjectDetailsRepository)
+        {
+            logger = _logger;
+            subjectDetailsRepository = _subjectDetailsRepository;
+        }
+
+        [HttpPost("InsertSubjectDetails")]
+        public async Task<IActionResult> InsertSubjectDetails(SubjectDetailsModel subjectDetailsModel)
+        {
+            if (subjectDetailsModel == null)
+            {
+                return BadRequest("Object is null");
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Invalid model object");
+            }
+
+            var result = await subjectDetailsRepository.InsertSubjectDetails(subjectDetailsModel);
+            if (result == ResponseCode.Success || result == ResponseCode.Updated)
+            {
+                return Ok(result);
+            }
+            return BadRequest("Failed to Save");
+        }
+
+        [HttpGet("getAllSubjectList")]
+        public async Task<IActionResult> GetSubjectDetailsList()
+        {
+            var data = await subjectDetailsRepository.GetSubjectDetailsList();
+            if (data != null)
+            {
+                return Ok(data);
+            }
+            return NotFound("No record found");
+        }
+    }
+}
