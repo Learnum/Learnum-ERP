35b02aadbb43bd5d91c26f8fb2f58c365be75310
chhaya.thosar@taxblock.in <chhaya.thosar@taxblock.in>
2024-07-01 10:31:46 +0530

    erp

--- Learnum.ERP.Shared/obj/Debug/net8.0/Learnum.ERP.Shared.AssemblyInfo.cs
+++ Learnum.ERP.Shared/obj/Debug/net8.0/Learnum.ERP.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Learnum.ERP.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b508006fce2c8b564fc820e42fd1443be2e7488")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25f414f048caeeba08e1a9b7811c9648d9132f50")]
 [assembly: System.Reflection.AssemblyProductAttribute("Learnum.ERP.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Learnum.ERP.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
