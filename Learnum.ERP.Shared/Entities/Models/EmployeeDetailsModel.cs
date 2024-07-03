0034f6a4b5a868ac8dba91929eec949acc736bc2
chhaya.thosar@taxblock.in <chhaya.thosar@taxblock.in>
2024-07-03 11:50:25 +0530

    api

--- Learnum.ERP.Shared/Entities/Models/EmployeeDetailsModel.cs
+++ Learnum.ERP.Shared/Entities/Models/EmployeeDetailsModel.cs
@@ -10,27 +10,20 @@ namespace Learnum.ERP.Shared.Entities.Models
    public class EmployeeDetailsModel : BaseModel
     {
         public long EmployeeId {  get; set; }
-
         public string EmployeeName { get; set; }
-
         public string EmployeeEmail { get; set; }
-
         public string EmployeePhone { get; set; }
-
         public long AadharNumber { get; set; }
-
         public DateTime DateOfBirth { get; set; }
-
         public string BloodGroup { get; set; }
         public string Gender { get; set; }
         public string Qualification { get; set; }
-
         public string AddressLine { get; set; }
         public string City { get; set; }
         public string State { get; set; }
-        public string PostalCode { get; set; }
+        public long PostalCode { get; set; }
         public string EmployeeRole { get; set; }
-        public string EmployeeStatus { get; set; }
+        public Boolean EmployeeStatus { get; set; }
     
 }
 }
--- Learnum.ERP.Shared/Entities/Models/ViewModel/BuisnessLeadModel/BuisnessLeadDetailsResponseModel.cs
+++ Learnum.ERP.Shared/Entities/Models/ViewModel/BuisnessLeadModel/BuisnessLeadDetailsResponseModel.cs
@@ -14,7 +14,12 @@ namespace Learnum.ERP.Shared.Entities.Models.ViewModel.BuisnessLeadModel
         public string Address { get; set; }
         public string CityOrDistrict { get; set; }
         public string StateOrProvince { get; set; }
-        public string PostalCode { get; set; }
+        public long PostalCode { get; set; }
         public string Country { get; set; }
+
+        public long? AddedBy { get; set; }
+        public DateTime? AddedDate { get; set; }
+        public long? UpdatedBy { get; set; }
+        public DateTime? UpdatedDate { get; set; }
     }
 }
