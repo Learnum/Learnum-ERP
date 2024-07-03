317f9e02566f1b814ceebbf10f717b9c97343e9a
chhaya.thosar@taxblock.in <chhaya.thosar@taxblock.in>
2024-07-03 11:30:32 +0530

    erp-api

--- Learnum.ERP.API/Controller/Business Lead/BusinessLeadDetailsController.cs
+++ Learnum.ERP.API/Controller/Business Lead/BusinessLeadDetailsController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Learnum.ERP.API.Controller.Counsellor_Dashboard;
+using Learnum.ERP.Repository.Master.Business_Lead_repo;
+using Learnum.ERP.Repository.Master.Counsellor_Dashboard_repo;
+using Learnum.ERP.Shared.Core;
+using Learnum.ERP.Shared.Entities.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Learnum.ERP.API.Controller.Business_Lead
@@ -7,5 +12,46 @@ namespace Learnum.ERP.API.Controller.Business_Lead
     [ApiController]
     public class BusinessLeadDetailsController : ControllerBase
     {
+        private readonly IBusinessLeadDetailsRepository businessLeadDetailsRepository;
+        private readonly ILogger<BusinessLeadDetailsController> logger;
+
+        public BusinessLeadDetailsController(
+            ILogger<BusinessLeadDetailsController> _logger,
+            IBusinessLeadDetailsRepository _businessLeadDetailsRepository)
+        {
+            logger = _logger;
+            businessLeadDetailsRepository = _businessLeadDetailsRepository;
+        }
+
+        [HttpPost("InsertBusinessLeadDetails")]
+        public async Task<IActionResult> InsertBusinessLeadDetails(BuisnessLeadDetailsModel businessLeadDetailsModel)
+        {
+            if (businessLeadDetailsModel == null)
+            {
+                return BadRequest("Object is null");
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Invalid model object");
+            }
+
+            var result = await businessLeadDetailsRepository.InsertBusinessLeadDetails(businessLeadDetailsModel);
+            if (result == ResponseCode.Success || result == ResponseCode.Updated)
+            {
+                return Ok(result);
+            }
+            return BadRequest("Failed to Save");
+        }
+
+        [HttpGet("getAllBusinessLeadList")]
+        public async Task<IActionResult> GetScheduleMeetingDetailsList()
+        {
+            var data = await businessLeadDetailsRepository.GetBuisnessLeadDetailsList();
+            if (data != null)
+            {
+                return Ok(data);
+            }
+            return NotFound("No record found");
+        }
     }
 }
---
+++ Learnum.ERP.Shared/Entities/Models/ViewModel/BuisnessLeadModel/BuisnessLeadDetailsResponseModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learnum.ERP.Shared.Entities.Models.ViewModel.BuisnessLeadModel
+{
+    public class BuisnessLeadDetailsResponseModel
+    { 
+        public long userID { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string CityOrDistrict { get; set; }
+        public string StateOrProvince { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+    }
+}
