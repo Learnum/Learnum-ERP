b6ccf45ec01e9df6754f3fb3b63eed8b3f69b03f
developer@cloudstine.com <developer@cloudstine.com>
2024-08-14 17:08:48 +0530

    erp-API

--- Learnum.ERP.Shared/Entities/Models/ViewModel/Masters/BatchesDetailsResponseModel.cs
+++ Learnum.ERP.Shared/Entities/Models/ViewModel/Masters/BatchesDetailsResponseModel.cs
@@ -10,30 +10,18 @@ namespace Learnum.ERP.Shared.Entities.Models.ViewModel
     {
         public long BatchId { get; set; }
         public long BranchId { get; set; }
-
         public long CourseId { get; set; }
-
         public long ClassroomId { get; set; }
-
         public string BatchName { get; set; }
-
         public string CourseName { get; set;}
-
         public string ClassroomName { get; set;}
-
         public string BranchName { get; set; }
         public string CourseFeesInstallment { get; set; }
-
         public string OneTimeCourseFees { get; set; }
-
         public DateTime StartOn { get; set; }
-
         public DateTime EndOn { get; set; }
-
         public Boolean IsActive { get; set; }
-
-        public DateTime StartTime { get; set; }
-
+        public DateTime StartTime { get; set; } 
         public DateTime EndTime { get; set; }
     }
 }
