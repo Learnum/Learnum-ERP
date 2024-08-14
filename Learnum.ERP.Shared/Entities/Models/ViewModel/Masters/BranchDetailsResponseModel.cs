4fa66f1ec62d135ded50fb8449cd96ea744027fb
developer@cloudstine.com <developer@cloudstine.com>
2024-08-14 10:49:46 +0530

    erp-ui

--- Learnum.ERP.Shared/Entities/Models/ViewModel/Masters/BranchDetailsResponseModel.cs
+++ Learnum.ERP.Shared/Entities/Models/ViewModel/Masters/BranchDetailsResponseModel.cs
@@ -14,9 +14,9 @@ namespace Learnum.ERP.Shared.Entities.Models.ViewModel
         public string Address { get; set; }
 
         public long StateId { get; set; }
-        public long CityId { get; set; }
+       
         public string City { get; set; }
-        public string State { get; set; }
+        public string StateName { get; set; }
         public long PostalCode { get; set; }
         public Boolean IsActive { get; set; }
     }
